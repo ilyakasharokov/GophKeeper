77d177092e0b4680240a0cf14b2c9c7e797af6e5
ikasharokov <ikasharokov>
2022-06-02 18:21:29 +0300

    cert

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -25,26 +25,6 @@ var (
 	BuildCommit  = "N/A"
 )
 
-func loadTLSCredentials() (credentials.TransportCredentials, error) {
-	// Load certificate of the CA who signed server's certificate
-	pemServerCA, err := ioutil.ReadFile("cert/ca-cert.pem")
-	if err != nil {
-		return nil, err
-	}
-
-	certPool := x509.NewCertPool()
-	if !certPool.AppendCertsFromPEM(pemServerCA) {
-		return nil, fmt.Errorf("failed to add server CA's certificate")
-	}
-
-	// Create the credentials and return it
-	config := &tls.Config{
-		RootCAs: certPool,
-	}
-
-	return credentials.NewTLS(config), nil
-}
-
 func main() {
 	fmt.Printf("Build version: %v\n", BuildVersion)
 	fmt.Printf("Build date: %v\n", BuildDate)
@@ -83,4 +63,25 @@ func handleSignals(cancel context.CancelFunc) {
 		cancel()
 		return
 	}
+}
+
+// loadTLSCredentials
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile("cert/ca-cert.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return credentials.NewTLS(config), nil
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,22 +30,6 @@ var (
 	BuildCommit  = "N/A"
 )
 
-func loadTLSCredentials() (credentials.TransportCredentials, error) {
-	// Load server's certificate and private key
-	serverCert, err := tls.LoadX509KeyPair("cert/server-cert.pem", "cert/server-key.pem")
-	if err != nil {
-		return nil, err
-	}
-
-	// Create the credentials and return it
-	config := &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-		ClientAuth:   tls.NoClientCert,
-	}
-
-	return credentials.NewTLS(config), nil
-}
-
 func main() {
 	fmt.Printf("Build version: %v\n", BuildVersion)
 	fmt.Printf("Build date: %v\n", BuildDate)
@@ -109,4 +93,21 @@ func main() {
 		cancel()
 	case <-ctx.Done():
 	}
+}
+
+// loadTLSCredentials
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load server's certificate and private key
+	serverCert, err := tls.LoadX509KeyPair("cert/server-cert.pem", "cert/server-key.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.NoClientCert,
+	}
+
+	return credentials.NewTLS(config), nil
 }
